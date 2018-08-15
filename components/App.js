06db0779d271bf48445adf5b7d8f315425540e29
Unni Le <unni.le@bouvet.no>
2018-08-15 11:03:17 +0200

    endringer fra tutorial 3 yt

--- components/App.js
+++ components/App.js
@@ -1,1 +1,14 @@
+import React from 'react'
 
+var App = React.createClass ({
+    render() {
+        return 
+            <div> This is definitely a React app now! </div>
+    }
+})
+
+// allows us to import this to client.js
+export default App
+
+ 
+// fjern () fra return hvis koden ikke funker 
